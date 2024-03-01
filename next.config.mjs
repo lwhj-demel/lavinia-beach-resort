c75b37adc84387fff5de14fcec525ca7de0729e3
MDeMel-Dev <160365493+lwhj-demel@users.noreply.github.com>
2024-03-01 12:59:16 +1100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-        unoptimized: true,
-        remotePatterns: [
-            {
-                hostname: 'swiperjs.com',
-            }
-        ],
-    }
+    output: 'export'
 };
 
 export default nextConfig;
