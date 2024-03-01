57a447d00476c8531e40951addfc53ebe5668aee
MDeMel-Dev <160365493+lwhj-demel@users.noreply.github.com>
2024-03-01 13:44:12 +1100

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // basePath: '/lavinia_beach_resort',
     output: 'export',
-    images: {
-        unoptimized: true
-    }
 };
 
 export default nextConfig;
