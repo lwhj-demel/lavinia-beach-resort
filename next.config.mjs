efbba4de57d474566c7eacd21bdcce7adf0b13ca
MDeMel-Dev <160365493+lwhj-demel@users.noreply.github.com>
2024-03-02 09:35:23 +1100

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 
+const isProd = process.env.NODE_ENV == 'production'
+
 const nextConfig = {
     output: 'export',
-    images: { unoptimized: true }
+    images: { unoptimized: true },
+    basePath: isProd ? "/lavinia-beach-resort" : '',
 };
 
 export default nextConfig;
